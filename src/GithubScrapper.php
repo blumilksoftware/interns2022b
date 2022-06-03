4256e55801327b7f055ac48fed37a1a8809dcd1b
Dagmara Skulimowska <104433434+dagmaraskulimowska@users.noreply.github.com>
2022-06-03 10:32:41 +0200

    #13 - retrieve more data (#16)
    * #13 - issue-modification-twitter-email
    
    * Update composer.lock
    
    * #13 - issue - modification - twitter - email
    
    * #13 - issue - modification - twitter - email

--- src/GithubScrapper.php
+++ src/GithubScrapper.php
@@ -12,6 +12,10 @@ class GithubScrapper
 {
     public const LOCATION = "location";
     public const URL = "url";
+    public const WEB = "website";
+    public const TWITTER = "twitter";
+    public const EMAIL = "email";
+    public const VERIFICATION = "verification";
 
     protected Crawler $crawler;
 
@@ -35,5 +39,29 @@ class GithubScrapper
     public function getRepositoryURL(): string
     {
         return "https://github.com/" . $this->name;
+    }
+
+    public function getWebsite(): string
+    {
+        $nodeWeb = $this->crawler->filterXPath("//a[@rel='nofollow']")->first();
+        return $nodeWeb->count() ? $nodeWeb->html() : "unknown";
+    }
+
+    public function getTwitter(): string
+    {
+        $nodeTwitter = $this->crawler->filterXPath("//a[contains(@href, 'twitter')]")->first();
+        return $nodeTwitter->count() ? $nodeTwitter->html() : "unknown";
+    }
+
+    public function getEmail(): string
+    {
+        $nodeEmail = $this->crawler->filterXPath("//a[@itemprop='email']")->first();
+        return $nodeEmail->count() ? $nodeEmail->html() : "unknown";
+    }
+
+    public function getVerification(): string
+    {
+        $nodeVerification = $this->crawler->filterXPath("//summary[@title='Label: Verified']")->first();
+        return $nodeVerification->count() ? $nodeVerification->html() : "not verified";
     }
 }
