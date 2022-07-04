01fba053b567d9a949a0ea125f4c1dad5814d41a
Dagmara Skulimowska <104433434+dagmaraskulimowska@users.noreply.github.com>
2022-07-04 14:15:15 +0200

    Update BreweryScrapper.php

--- src/BreweryScrapper.php
+++ src/BreweryScrapper.php
@@ -7,7 +7,6 @@ namespace Interns2022B;
 class BreweryScrapper
 {
     public const EXIT = "exit";
-    protected const TABLE_DIRECTORY = __DIR__ . "/../tests/stubs/table.json";
 
     protected array $data = [];
 
@@ -20,35 +19,5 @@ class BreweryScrapper
     public function getData(): array
     {
         return $this->data;
-    }
-
-    public function putData(string $keyName): void
-    {
-        foreach ($this->data as $rowValue) {
-            if ($rowValue["provider"] === $keyName) {
-                $this->toFile[] = $rowValue;
-            }
-        }
-    }
-
-    public function rebuildCache(): void
-    {
-        $timeStamp = ["last Build:", "[", date("Y-m-d h:i:s"), "]", "\n"];
-
-        $logDirectory = __DIR__ . "/../tests/stubs";
-
-        if (!file_exists($logDirectory)) {
-            mkdir($logDirectory);
-        }
-
-        $filename = $logDirectory . "/cacheChanges" . ".log";
-        file_put_contents($filename, implode(" ", $timeStamp));
-
-        $provider = $this->getProviders();
-        foreach ($provider as $value) {
-            $this->putData($value);
-        }
-        file_put_contents(self::TABLE_DIRECTORY, json_encode($this->toFile, JSON_THROW_ON_ERROR | JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE), LOCK_EX);
-        file_put_contents(self::TABLE_DIRECTORY, "\n", FILE_APPEND);
     }
 }
