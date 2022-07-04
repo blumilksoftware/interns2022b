6f374839cb77226b8af448743997b9f2966a06dc
Aleksandra Kozubal <104600942+AleksandraKozubal@users.noreply.github.com>
2022-07-04 14:01:43 +0200

    #21 - Rebuild cache (#27)
    * #21 - Rebuild cache
    
    * Update BreweryScrapper.php
    
    * Update BreweryScrapper.php
    
    * #21-Rebuild-cache
    
    Suggestions are applied, didn't use example cache for now
    
    * Update BreweryScrapper.php
    
    * Update BreweryScrapper.php
    
    * Update BreweryScrapper.php

--- src/BreweryScrapper.php
+++ src/BreweryScrapper.php
@@ -9,13 +9,16 @@ class BreweryScrapper
     public const SHOW = "show";
     public const LIST = "list";
     public const CLEAR = "clear";
+    public const BUILD = "build";
     public const EXIT = "exit";
+    protected const TABLE_DIRECTORY = __DIR__ . "/../tests/stubs/table.json";
 
     public array $data = [];
+    public array $toFile = [];
 
     public function collectData(): void
     {
-        $handle = file_get_contents(__DIR__ . "/../tests/stubs/table.json");
+        $handle = file_get_contents(self::TABLE_DIRECTORY);
         $this->data = json_decode($handle, associative: true);
     }
 
@@ -24,33 +27,61 @@ class BreweryScrapper
         $name = readline("Please provide brewery or city name:");
 
         $choosenData = [];
-        for ($row = 0; $row < count($this->data); $row++) {
-            if (in_array($name, $this->data[$row], strict: true)) {
-                $choosenData[] = $this->data[$row];
+        foreach ($this->data as $rowValue) {
+            if (in_array($name, $rowValue, strict: true)) {
+                $choosenData[] = $rowValue;
             }
         }
         return $choosenData;
-    }    
+    }
 
     public function getProviders(): array
     {
         $providers = [];
-        for ($row = 0; $row < count($this->data); $row++) {
-            $providers[] = $this->data[$row]["Provider"];
+        foreach ($this->data as $row => $rowValue) {
+            $providers[] = $this->data[$row]["provider"];
         }
         return $providers;
     }
 
     public function clearCache(): string
     {
-        $handle = __DIR__ . "/../tests/stubs/table.json";
-
-        if (unlink($handle)) {
+        if (unlink(self::TABLE_DIRECTORY)) {
             $message = "File was deleted successfully";
         } else {
             $message = "File doesn't exist";
         }
 
         return $message;
+    }
+
+    public function putData(string $keyName): void
+    {
+        foreach ($this->data as $rowValue) {
+            if ($rowValue["provider"] === $keyName) {
+                $this->toFile[] = $rowValue;
+            }
+        }
+    }
+
+    public function rebuildCache(): void
+    {
+        $timeStamp = ["last Build:", "[", date("Y-m-d h:i:s"), "]", "\n"];
+
+        $logDirectory = __DIR__ . "/../tests/stubs";
+
+        if (!file_exists($logDirectory)) {
+            mkdir($logDirectory);
+        }
+
+        $filename = $logDirectory . "/cacheChanges" . ".log";
+        file_put_contents($filename, implode(" ", $timeStamp));
+
+        $provider = $this->getProviders();
+        foreach ($provider as $value) {
+            $this->putData($value);
+        }
+        file_put_contents(self::TABLE_DIRECTORY, json_encode($this->toFile, JSON_THROW_ON_ERROR | JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE), LOCK_EX);
+        file_put_contents(self::TABLE_DIRECTORY, "\n", FILE_APPEND);
     }
 }
